ed3e90e79887fe75924cefd196923a9eb86220cd
Giovani Silqueira Santos <giovani0skeey@gmail.com>
2019-11-17 01:45:56 -0300

    made app structure and connection with firebase

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:coffee_list/screens/authenticate/authenticate.dart';
+import 'package:coffee_list/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Authenticate();
+  }
+}
